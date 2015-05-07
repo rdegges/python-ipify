6320319652ef1a1f8eda8680106b6908053465d8
Randall Degges <r@rdegges.com>
2015-05-06 21:54:30 -0700

    Refactoring settings into it's own helper module.

--- ipify/ipify.py
+++ ipify/ipify.py
@@ -6,38 +6,12 @@ The module holds the main ipify library implementation.
 """
 
 
-from platform import mac_ver, win32_ver, linux_distribution, system
-from sys import version_info as vi
-
 from backoff import expo, on_exception
 from requests import get
 from requests.exceptions import RequestException
 
-from . import __version__
 from .exceptions import ConnectionError, ServiceError
-
-
-# Globals
-API_URI = 'https://api.ipify.org'
-OS_VERSION_INFO = {
-    'Linux': '%s' % (linux_distribution()[0]),
-    'Windows': '%s' % (win32_ver()[0]),
-    'Darwin': '%s' % (mac_ver()[0]),
-}
-
-# The user-agent string is provided so that I can (eventually) keep track of
-# what libraries to support over time.  EG: Maybe the service is used primarily
-# by Windows developers, and I should invest more time in improving those
-# integrations.
-USER_AGENT = 'python-ipify/%s python/%s %s/%s' % (
-    __version__,
-    '%s.%s.%s' % (vi.major, vi.minor, vi.micro),
-    system(),
-    OS_VERSION_INFO.get(system(), ''),
-)
-
-# The maximum amount of tries to attempt when making API calls.
-MAX_TRIES = 3
+from .settings import API_URI, MAX_TRIES, USER_AGENT
 
 
 @on_exception(expo, RequestException, max_tries=MAX_TRIES)
